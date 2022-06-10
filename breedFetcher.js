c4d254833452b2f9276dcaa667459ee0d421e4c3
Timi Alabi <alabitimi65@gmail.com>
2022-06-10 16:05:53 +0000

    breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,24 @@
 const request = require("request");
+
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    const data = JSON.parse(body);
+    console.log(data);
+
+    if (Array.isArray(data) && data.length) {
+      return callback(null, data[0].description);
+    }
+    if (Array.isArray(data) && !data.length) {
+      return callback(null, null);
+    }
+  });
+};
+
+fetchBreedDescription(Siberian);
+
+module.exports = { fetchBreedDescription };
